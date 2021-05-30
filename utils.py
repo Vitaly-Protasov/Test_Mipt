a45ff512891cec385d6c95bef7867942c76ad0ae
Vitaly Protasov <izakladno@yandex.ru>
2021-05-30 17:11:17 +0300

    added utils

---
+++ utils.py
@@ -0,0 +1,39 @@
+import re
+import pandas as pd
+from gensim import corpora,models,similarities
+from gensim.utils import tokenize
+import nltk
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+import pymorphy2
+from tqdm import tqdm
+import os
+from pathlib import PurePath
+from typing import List
+
+
+def text_preprocess(text: str) -> List[str]:
+    # lemmatize
+    morph = pymorphy2.MorphAnalyzer()
+    stopwords_set = set(stopwords.words('russian'))
+    tokenized_text = list(tokenize(text, lowercase=True))
+
+    cleared_text_list = []
+    for word in tokenized_text:
+        if word not in stopwords_set and len(word) > 1 and len(word) <= 20:
+            cleared_word = re.sub(r'[^\w]', '', word).strip()
+            word_lemma = morph.parse(cleared_word)[0].normal_form
+            cleared_text_list.append(word_lemma)
+    return cleared_text_list
+
+
+def make_df_from_files(folder_names: List[str]) -> pd.DataFrame:
+    df = {'file_text': [], 'file_class': []}
+    for folder_name in folder_names:
+        for file_name in os.listdir(folder_name):
+            file_path = PurePath(folder_name, file_name)
+            f = open(file_path)
+            f_text = ' '.join(list(f))
+            df['file_text'].append(f_text)
+            df['file_class'].append(folder_name)
+    return pd.DataFrame(df)
